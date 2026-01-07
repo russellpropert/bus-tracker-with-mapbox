63631cbdba218a922804987f5b1372f75d571c32
Russell Propert <russellpropert@gmail.com>
2026-01-07 01:01:45 -0500

    Adding If-Modified-Since to MBTA query headers and improving variable names.

--- mapMarkers.js
+++ mapMarkers.js
@@ -11,54 +11,59 @@ const map = new mapboxgl.Map({
   zoom: 13
 });
 
-const updateMarkers = (data) => {
-  data = data.data;
+const updateMarkers = (vehicles) => {
+  const vehicleData = vehicles.data;
 
-  const len = Math.max(data.length, markers.length);
+  const len = Math.max(vehicleData.length, markers.length);
 
   for (let i = 0; i < len; i++) {
     if (!markers[i]) {
       markers[i] = new mapboxgl.Marker()
-        .setLngLat([data[i].attributes.longitude, data[i].attributes.latitude])
+        .setLngLat([vehicleData[i].attributes.longitude, vehicleData[i].attributes.latitude])
         .addTo(map);
-    } else if (i >= data.length) {
+    } else if (i >= vehicleData.length) {
       markers[i].remove();
     } else {
-      markers[i].setLngLat([data[i].attributes.longitude, data[i].attributes.latitude]);
+      markers[i].setLngLat([vehicleData[i].attributes.longitude, vehicleData[i].attributes.latitude]);
     }
   }
 };
 
-let mbtaLastModified = null;
 const mbta_url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
 const mbta_options = {
   headers: {
-    'X-API-Key': MBTA_API_KEY,
-    'If-Modified-Since': mbtaLastModified
+    'X-API-Key': MBTA_API_KEY
   }
 }
+let vehicles = { data: [] };
 
-const getBuses = async () => {
+const getVehicles = async () => {
+  
   try {
     const response = await fetch(mbta_url, mbta_options);
-    mbtaLastModified = response.headers.get('Last-Modified');
-    const data = await response.json();
-    return data;
+    const lastModified = response.headers.get('Last-Modified');
+    if (lastModified !== mbta_options.headers['If-Modified-Since']) vehicles = await response.json();
+    if (lastModified) mbta_options.headers['If-Modified-Since'] = lastModified;
+    return vehicles;
   } catch (error) {
-    console.error(`There was a problem getting the bus data from ${url}`);
+    console.error(`There was a problem getting the bus data from ${mbta_url}`);
+    console.error(error);
+    return vehicles;
   }
 }
 
-getBuses()
-  .then((data) => updateMarkers(data))
-  .catch((error) => {
-    console.error(error);
-  });
-
-setInterval(() => {
-  getBuses()
-    .then((data) => updateMarkers(data))
+const getVehiclesAndUpdateMarkers = () => {
+  getVehicles()
+    .then(
+      (vehicles) => updateMarkers(vehicles)
+    )
     .catch((error) => {
       console.error(error);
     });
-}, 1000);
+}
+
+getVehiclesAndUpdateMarkers();
+
+setInterval(() => {
+  getVehiclesAndUpdateMarkers();
+}, 1500);
